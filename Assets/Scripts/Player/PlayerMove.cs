f4e69c2ad6c477af6e454d510d89c4a68578042d
SJW1737 <sic470212@naver.com>
2025-07-24 19:50:13 +0900

    [feat] 플레이어 움직임, 오브젝트 생성

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Vector2 minLimit;
+    public Vector2 maxLimit;
+
+    private Rigidbody2D _rigidBody;
+    private Vector2 movement;
+
+    void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        movement = new Vector2(horizontal, vertical).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 newPosition = _rigidBody.position + movement * moveSpeed * Time.fixedDeltaTime;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, minLimit.x, maxLimit.x);
+        newPosition.y = Mathf.Clamp(newPosition.y, minLimit.y, maxLimit.y);
+
+        _rigidBody.MovePosition(newPosition);
+    }
+}
+
+
